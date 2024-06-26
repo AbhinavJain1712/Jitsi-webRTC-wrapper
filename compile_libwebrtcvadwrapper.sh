17d6e0e1f9b4dc258d4746bbbc92bd92038e0f48
Abhinav Jain <btech10637.21@bitmesra.ac.in>
2024-06-27 00:16:28 +0530

    first_commit

--- compile_libwebrtcvadwrapper.sh
+++ compile_libwebrtcvadwrapper.sh
@@ -31,30 +31,3 @@ cd ..
 
 
 
-
-
-
-
-
-
-
-for x86-64
-
-#!/usr/bin/env bash
-set -e
-
-# Create build directory and navigate into it
-mkdir -p cmake-build
-cd cmake-build
-
-# Configure the build system for x86_64 architecture
-cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 ..
-
-# Build the project
-make
-
-# Copy the built library to the appropriate directory
-cp libwebrtcvadwrapper.dylib ../lib/native/darwin-x86-64/.
-
-# Navigate back to the original directory
-cd ..
